af0f8beaaa40f0ce23d5bcd43bd800eb1314cb50
73408 <73408@DESKTOP-19FKM9P.lan1>
2019-09-02 00:04:30 -0400

    novice check

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -8,7 +8,26 @@ public class A1Novice {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
-		
+		int total_number = scan.nextInt();
+				
+		//String[] first_names = new String[total_number];
+				
+		//String[] last_names = new String[total_number];
+				
+				
+				
+		for (int i=0; i<total_number; i++) {
+			String first_name = scan.next();
+			String last_name = scan.next();
+		    int number_of_items = scan.nextInt();
+		    double total_cost = 0;
+				    
+			for (int j=0; j<number_of_items; j++) {
+				total_cost += scan.nextInt() * scan.nextDouble();
+				String item = scan.next();
+			}
+				    
+		    System.out.println(first_name.charAt(0) + ". " + last_name + ": " + total_cost);
+		}	
 	}
 }

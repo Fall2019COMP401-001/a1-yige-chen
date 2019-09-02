232e5a95a883129428b5aefffdcd07f6904200bb
73408 <73408@DESKTOP-19FKM9P.lan1>
2019-09-02 00:39:30 -0400

    notive second version check

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,25 +9,32 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		int total_number = scan.nextInt();
+		
 				
-		//String[] first_names = new String[total_number];
+		String[] first_names = new String[total_number];
 				
-		//String[] last_names = new String[total_number];
-				
-				
+		String[] last_names = new String[total_number];
+		
+		double[] total_cost = new double[total_number];
 				
 		for (int i=0; i<total_number; i++) {
-			String first_name = scan.next();
-			String last_name = scan.next();
+			first_names[i] = scan.next();
+			last_names[i] = scan.next();
 		    int number_of_items = scan.nextInt();
-		    double total_cost = 0;
 				    
 			for (int j=0; j<number_of_items; j++) {
-				total_cost += scan.nextInt() * scan.nextDouble();
+				int number = scan.nextInt();
 				String item = scan.next();
+				double price = scan.nextDouble();
+				total_cost[i] += number * price;
+				
 			}
-				    
-		    System.out.println(first_name.charAt(0) + ". " + last_name + ": " + total_cost);
+		}
+		
+		scan.close();
+		
+		for (int i=0; i<total_number; i++) {
+			System.out.println(first_names[i].charAt(0) + ". " + last_names[i] + ": " + total_cost[i]);
 		}
 		
 	}
